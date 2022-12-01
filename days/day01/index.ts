71bcfb4e530d926201a2086bc27602d8cfa3774b
Ismael Barragan <ismael.barraganlopez@salesloft.com>
2022-12-01 12:28:09 -0600

    Optimize day01

--- days/day01/index.ts
+++ days/day01/index.ts
@@ -7,25 +7,9 @@ const part01 = readInput('days/day01/input02', '\n\n').map((elf: string) => {
   return elf.split('\n').map(Number);
 });
 
-function processPart01() {
-  const elfsTotalCalories = part01.map((calories) => {
-    return calories.reduce((acc, calorie) => acc + calorie, 0);
-  });
+const caloriesPerElf = part01.map((calories) => calories.reduce((acc, calorie) => acc + calorie, 0));
 
-  elfsTotalCalories.sort((a, b) => b - a);
+caloriesPerElf.sort((a, b) => b - a);
 
-  return elfsTotalCalories[0];
-}
-
-function processPart02() {
-  const elfsTotalCalories = part01.map((calories) => {
-    return calories.reduce((acc, calorie) => acc + calorie, 0);
-  });
-
-  elfsTotalCalories.sort((a, b) => b - a);
-
-  return elfsTotalCalories[0] + elfsTotalCalories[1] + elfsTotalCalories[2];
-}
-
-process.stdout.write(`Part 01: ${processPart01()}\n`);
-process.stdout.write(`Part 01: ${processPart02()}\n`);
+process.stdout.write(`Part 01: ${caloriesPerElf.at(0)}\n`);
+process.stdout.write(`Part 01: ${caloriesPerElf.at(0) + caloriesPerElf.at(1) + caloriesPerElf.at(2)}\n`);
